e72c2f54475fc57889952efedbab59ccb50b394c
Pablo C. Leite <pablo.leite@gmail.com>
2019-02-18 10:25:00 -0300

    Refactoring states to use scriptable objects. Code is now a lot cleaner!

---
+++ Assets/State.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+    //TextArea will determine the size of the field displayed on the Unity's inspector.
+    [TextArea(14,10)] [SerializeField] string storyText;
+    [SerializeField] public State[] nextStates;
+
+    public string GetStateStory() {
+        return storyText;
+    }
+
+    public State[] GetNextStates() {
+        return nextStates;
+    }
+}
---
+++ Assets/TextAdventureGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAdventureGame : MonoBehaviour {
+
+    [SerializeField] public Text text;
+    [SerializeField] public State startingState;
+
+    private State currentState;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentState = startingState;
+        text.text = currentState.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ManageState();
+    }
+
+    private void ManageState() {
+        var nextStates = currentState.GetNextStates();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            currentState = nextStates[0];
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            currentState = nextStates[1];
+        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            currentState = nextStates[2];
+        }
+
+        text.text = currentState.GetStateStory();
+    }
+}
